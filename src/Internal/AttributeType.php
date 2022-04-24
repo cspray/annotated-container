e79c5740df5cbc9f21375b9f79b45a923c87ac5b
Charles Sprayberry <me@cspray.io>
2022-04-24 10:42:55 -0400

    Declare strict types

--- src/Internal/AttributeType.php
+++ src/Internal/AttributeType.php
@@ -1,17 +1,10 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Cspray\AnnotatedContainer\Internal;
 
-use Cspray\AnnotatedContainer\Attribute\InjectEnv;
-use Cspray\AnnotatedContainer\Attribute\InjectScalar;
-use Cspray\AnnotatedContainer\Attribute\InjectService;
 use Cspray\AnnotatedContainer\Attribute\Service;
 use Cspray\AnnotatedContainer\Attribute\ServiceDelegate;
 use Cspray\AnnotatedContainer\Attribute\ServicePrepare;
-use Cspray\AnnotatedContainer\Attribute\ThirdPartyInjectScalar;
-use Cspray\AnnotatedContainer\Attribute\ThirdPartyService;
-use Cspray\AnnotatedContainer\Attribute\ThirdPartyServiceDelegate;
-use Cspray\AnnotatedContainer\Attribute\ThirdPartyServicePrepare;
 
 /**
  * @Internal
