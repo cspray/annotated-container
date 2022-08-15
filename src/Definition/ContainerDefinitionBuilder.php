af7aea234f87eb1b626a5d0c845405d4b752ce5d
Charles Sprayberry <cspray@users.noreply.github.com>
2022-08-14 20:50:27 -0400

    Remove deprecated classes and methods (#224)

--- src/Definition/ContainerDefinitionBuilder.php
+++ src/Definition/ContainerDefinitionBuilder.php
@@ -2,8 +2,6 @@
 
 namespace Cspray\AnnotatedContainer\Definition;
 
-use Cspray\AnnotatedContainer\Exception\ContainerDefinitionMergeException;
-
 /**
  * The preferred method for constructing ContainerDefinition instances.
  */
@@ -79,47 +77,13 @@ final class ContainerDefinitionBuilder {
         ) implements ContainerDefinition {
 
             public function __construct(
-                private array $serviceDefinitions,
-                private array $aliasDefinitions,
-                private array $servicePrepareDefinitions,
-                private array $injectDefinitions,
-                private array $serviceDelegateDefinitions,
-                private array $configurationDefinitions
+                private readonly array $serviceDefinitions,
+                private readonly array $aliasDefinitions,
+                private readonly array $servicePrepareDefinitions,
+                private readonly array $injectDefinitions,
+                private readonly array $serviceDelegateDefinitions,
+                private readonly array $configurationDefinitions
             ) {}
-
-            public function merge(ContainerDefinition $containerDefinition) : ContainerDefinition {
-                $merged = clone $this;
-                foreach ($containerDefinition->getServiceDefinitions() as $serviceDefinition) {
-                    if ($this->hasServiceDefinition($serviceDefinition)) {
-                        throw new ContainerDefinitionMergeException(sprintf(
-                            'The ContainerDefinition already has a ServiceDefinition for %s.',
-                            $serviceDefinition->getType()->getName()
-                        ));
-                    }
-                    $merged->serviceDefinitions[] = $serviceDefinition;
-                }
-
-                foreach ($containerDefinition->getAliasDefinitions() as $aliasDefinition) {
-                    if ($this->hasAliasDefinition($aliasDefinition)) {
-                        throw new ContainerDefinitionMergeException(sprintf(
-                            'The ContainerDefinition already has an AliasDefinition for %s aliased to %s.',
-                            $aliasDefinition->getAbstractService()->getName(),
-                            $aliasDefinition->getConcreteService()->getName()
-                        ));
-                    }
-                    $merged->aliasDefinitions[] = $aliasDefinition;
-                }
-
-                foreach ($containerDefinition->getServicePrepareDefinitions() as $servicePrepareDefinition) {
-                    $merged->servicePrepareDefinitions[] = $servicePrepareDefinition;
-                }
-
-                foreach ($containerDefinition->getServiceDelegateDefinitions() as $serviceDelegateDefinition) {
-                    $merged->serviceDelegateDefinitions[] = $serviceDelegateDefinition;
-                }
-
-                return $merged;
-            }
 
             public function getServiceDefinitions(): array {
                 return $this->serviceDefinitions;
@@ -143,26 +107,6 @@ final class ContainerDefinitionBuilder {
 
             public function getConfigurationDefinitions() : array {
                 return $this->configurationDefinitions;
-            }
-
-            private function hasServiceDefinition(ServiceDefinition $serviceDefinition) : bool {
-                foreach ($this->serviceDefinitions as $storedDefinition) {
-                    if ($serviceDefinition->equals($storedDefinition)) {
-                        return true;
-                    }
-                }
-
-                return false;
-            }
-
-            private function hasAliasDefinition(AliasDefinition $aliasDefinition) : bool {
-                foreach ($this->aliasDefinitions as $storedDefinition) {
-                    if ($aliasDefinition->equals($storedDefinition)) {
-                        return true;
-                    }
-                }
-
-                return false;
             }
         };
     }
