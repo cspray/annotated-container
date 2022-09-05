8b78db3caaba03fcb3c00da6e1e8f7e480585635
Charles Sprayberry <cspray@users.noreply.github.com>
2022-09-05 18:07:24 -0400

    Add comments on Attributes for how to implement them (#263)

--- src/Attribute/InjectAttribute.php
+++ src/Attribute/InjectAttribute.php
@@ -2,14 +2,37 @@
 
 namespace Cspray\AnnotatedContainer\Attribute;
 
-use UnitEnum;
-
+/**
+ * Can be implemented by your own classes to act as an Attribute for defining values that can be injected into service
+ * method parameters or a Configuration property.
+ *
+ * The class that implements this interface should also be marked as an Attribute; it can be repeatable and should be
+ * able to target a method parameter and a class property.
+ */
 interface InjectAttribute {
 
+    /**
+     * A serializable value that will be injected into the parameter or property, as appropriate for the target.
+     *
+     * @return mixed
+     */
     public function getValue() : mixed;
 
+    /**
+     * A list of profiles that have to be active for this Attribute to be used.
+     *
+     * If an empty list is returned the Attribute will be implicitly assigned the 'default' profile.
+     *
+     * @return list<string>
+     */
     public function getProfiles() : array;
 
+    /**
+     * If the actual value for this injection should come from a ParameterStore implementation then return its name,
+     * otherwise return null.
+     *
+     * @return string|null
+     */
     public function getFrom() : ?string;
 
 }
--- src/Attribute/ServiceAttribute.php
+++ src/Attribute/ServiceAttribute.php
@@ -2,17 +2,36 @@
 
 namespace Cspray\AnnotatedContainer\Attribute;
 
-use Cspray\Typiphy\ObjectType;
-
+/**
+ * Can be implemented by your own classes to act as an Attribute for defining your own Services wired into your
+ * Container.
+ *
+ * The class that implements this interface should also be marked as an Attribute; it should not be repeatable and
+ * should be able to only target the class construct.
+ */
 interface ServiceAttribute {
 
     /**
+     * A list of profiles that have to be active for this Attribute to be wired into the Container.
+     *
+     * If an empty list is returned the Attribute will be implicitly assigned the 'default' profile.
+     *
      * @return list<string>
      */
     public function getProfiles() : array;
 
+    /**
+     * Return whether this concrete service should be considered the primary service when resolving an abstract alias.
+     *
+     * @return bool
+     */
     public function isPrimary() : bool;
 
+    /**
+     * Return a string to fetch a service by an arbitrary name in addition to its fully qualified type.
+     *
+     * @return string|null
+     */
     public function getName() : ?string;
 
 }
